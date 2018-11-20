c6806786e237371b423b871382b600def6ed644a
Baptiste Manach <baptistemanach@gmail.com>
2018-11-20 15:03:13 +0100

    Initial commit

---
+++ app/src/main/java/fontys/andr2/friendsfinder/MainActivity.java
@@ -0,0 +1,13 @@
+package fontys.andr2.friendsfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
