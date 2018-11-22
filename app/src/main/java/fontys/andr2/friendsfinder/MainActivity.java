9fd10e6db960f1414f762cd1608a8821284fe401
Baptiste Manach <baptistemanach@gmail.com>
2018-11-22 11:13:35 +0100

    Adding a button to start Map

--- app/src/main/java/fontys/andr2/friendsfinder/MainActivity.java
+++ app/src/main/java/fontys/andr2/friendsfinder/MainActivity.java
@@ -3,14 +3,28 @@ package fontys.andr2.friendsfinder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    void launchMapActivity(){
         startActivity(new Intent(this, MapsActivity.class));
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_start_map:
+                launchMapActivity();
+                break;
+        }
     }
 }
