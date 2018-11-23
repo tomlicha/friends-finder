e1e50195d0307f396dee576ea52eee8523cc95fd
Gary Tanujaya <gary.tanujaya@gmail.com>
2018-11-23 09:18:42 +0100

    Added Google OAuth

--- app/src/main/java/fontys/andr2/friendsfinder/MainActivity.java
+++ app/src/main/java/fontys/andr2/friendsfinder/MainActivity.java
@@ -1,22 +1,87 @@
 package fontys.andr2.friendsfinder;
 
+import android.accounts.AccountManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Used for OAuth - Start
+    private static final String TAG = "IdTokenActivity";
+    private static final int RC_GET_TOKEN = 9002;
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private ImageView profilePic;
+    private TextView fullnameTextView;
+    private TextView emailTextView;
+    private TextView mIdTokenTextView;
+    //Used for OAuth - End
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Used for OAuth - Start
+        //Show Token
+        profilePic = findViewById(R.id.profile_image);
+        fullnameTextView = findViewById(R.id.name_view);
+        emailTextView = findViewById(R.id.email_view);
+        mIdTokenTextView = findViewById(R.id.token_detail);
+
+        //OnClickListener
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+
+        /*
+        //Sign-in
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        */
+
+        //Sign-In-token
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                //.requestIdToken(getString(R.string.server_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setColorScheme(SignInButton.COLOR_LIGHT);
+
+        //Used for OAuth - End
     }
 
     void launchMapActivity(){
         startActivity(new Intent(this, MapsActivity.class));
-
     }
 
     @Override
@@ -25,6 +90,172 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_start_map:
                 launchMapActivity();
                 break;
+            case R.id.sign_in_button:
+                getIdToken();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
         }
     }
+
+    //Used for OAuth - Start
+
+
+    //OnStart
+    public void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        updateUI(account);
+    }
+
+
+
+    /*
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    */
+
+    /*
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    */
+
+    //Get Token ID
+    private void getIdToken() {
+        //For GoogleSignInOption, show several email address.
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_GET_TOKEN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_GET_TOKEN) {
+            // [START get_id_token]
+            // This task is always completed immediately, there is no need to attach an
+            // asynchronous listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+            // [END get_id_token]
+        }
+    }
+
+    private void handleSignInResult(@NonNull Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String idToken = account.getIdToken();
+
+            // TODO(developer): send ID Token to server and validate
+
+            //updateUI(account);
+        } catch (ApiException e) {
+            Log.w(TAG, "handleSignInResult:error", e);
+            //updateUI(null);
+        }
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void updateUI(@Nullable GoogleSignInAccount account) {
+        if (account != null) {
+            Uri url = account.getPhotoUrl();
+            String imgUrl;
+            if (url != null){
+                imgUrl= url.toString();
+            }
+            else{
+                imgUrl= "null";
+            }
+            //new DownloadImageTask(profilePic).execute(imgUrl);
+            fullnameTextView.setText("Name: " + account.getDisplayName());
+            emailTextView.setText("Email: " + account.getEmail());
+
+            String idToken = account.getIdToken();
+            mIdTokenTextView.setText("Photo URL:" + imgUrl);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+        }
+        else {
+            fullnameTextView.setText("Name: None");
+            emailTextView.setText("Email: None");
+            mIdTokenTextView.setText("Token: Null");
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String,Void,Bitmap> {
+        ImageView imageView;
+
+        public DownloadImageTask(ImageView imageView){
+            this.imageView = imageView;
+        }
+
+        /*
+            doInBackground(Params... params)
+                Override this method to perform a computation on a background thread.
+         */
+        protected Bitmap doInBackground(String...urls){
+            String urlOfImage = urls[0];
+            Bitmap logo = null;
+            try{
+                InputStream is = new URL(urlOfImage).openStream();
+                /*
+                    decodeStream(InputStream is)
+                        Decode an input stream into a bitmap.
+                 */
+                logo = BitmapFactory.decodeStream(is);
+            }catch(Exception e){ // Catch the download exception
+                e.printStackTrace();
+            }
+            return logo;
+        }
+
+        /*
+            onPostExecute(Result result)
+                Runs on the UI thread after doInBackground(Params...).
+         */
+        protected void onPostExecute(Bitmap result){
+            imageView.setImageBitmap(result);
+        }
+    }
+    //Used for OAuth - End
 }
