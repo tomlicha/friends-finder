566ce1214bbe0a5305942e80eff903eba983204d
Gary Tanujaya <gary.tanujaya@gmail.com>
2018-12-05 22:44:38 +0100

    Added Custom List View

--- app/src/main/java/fontys/andr2/friendsfinder/Fragments/FriendsFragment.java
+++ app/src/main/java/fontys/andr2/friendsfinder/Fragments/FriendsFragment.java
@@ -1,17 +1,27 @@
 package fontys.andr2.friendsfinder.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import fontys.andr2.friendsfinder.MainActivity;
 import fontys.andr2.friendsfinder.R;
 
 
 public class FriendsFragment extends Fragment {
+
+    ListView lv;
 
     public FriendsFragment() {
         // Required empty public constructor
@@ -22,9 +32,46 @@ public class FriendsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+        lv= (ListView) view.findViewById(R.id.friendView);
+        lv.setAdapter(new customAdapter());
 
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        return view;
     }
+
+    class customAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 1;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View v = getLayoutInflater().inflate(R.layout.friends_item, null);
+            //Log.e("jumjumjum", Integer.toString(array1.length()));
+
+            TextView fullname = (TextView) v.findViewById(R.id.nameView);
+            TextView online = (TextView) v.findViewById(R.id.onlineView);
+            TextView city = (TextView) v.findViewById(R.id.cityView);
+            fullname.setText("Gary");
+            online.setText("Offline");
+            city.setText("Eindhoven");
+
+            return v;
+        }
+    }
+
+
 
 
 }
