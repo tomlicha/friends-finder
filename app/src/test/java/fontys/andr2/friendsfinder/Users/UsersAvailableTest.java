e4f4449e4e199b13a367b875acec71d2328aca3a
Truong <n4truong@gmail.com>
2018-12-18 18:57:00 +0100

    User Available Test Skeleton Done

---
+++ app/src/test/java/fontys/andr2/friendsfinder/Users/UsersAvailableTest.java
@@ -0,0 +1,42 @@
+package fontys.andr2.friendsfinder.Users;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class UsersAvailableTest {
+
+    private HashMap<String,User> usersAvailable;
+    private UsersAvailable.RefreshListener refreshListener;
+
+    //----------------------------------------------------------------//
+
+    @Test
+    public void userListIsNotNull(){
+
+    }
+
+    @Test
+    public void userCanBeAddToList(){
+
+    }
+
+    @Test
+    public void userCanBeRetrievedFromEmail(){
+
+    }
+
+    @Test
+    public void RefreshListenerIsCalledInSetRefresh(){
+
+    }
+
+    @Test
+    public void listenerCanBeSet(){
+
+    }
+
+}
